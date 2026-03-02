5db163a5e7d24206bbd4de2cf4cf2861c9757d40
akankshakarhad <akankshakarhad@gmail.com>
2026-03-02 17:15:46 +0530

    chnages related to registration page

--- backend/src/database/migrate.sql
+++ backend/src/database/migrate.sql
@@ -7,3 +7,6 @@ ALTER TABLE users ADD COLUMN IF NOT EXISTS course VARCHAR(200) NOT NULL DEFAULT 
 
 -- Migration: add participant_type to users
 ALTER TABLE users ADD COLUMN IF NOT EXISTS participant_type VARCHAR(20) NOT NULL DEFAULT '';
+
+-- Migration: update Project Display max_members to 5
+UPDATE events SET max_members = 5 WHERE title = 'Project Display';
